75da785299bac8467a80fa9f04f9eeb6e3bff393
Neo Gullberg <neogullberg04@gmail.com>
2021-11-30 12:52:27 +0100

    fixed grammar and empty space

--- text.py
+++ text.py
@@ -1,3 +1,11 @@
+travel_messages = [
+    "The path ahead is crooked.",
+    "You feel as if someone is watching you.",
+    "You feel like you been here before.",
+    "This place stinks.",
+    "The ground is covered by a suspicious sludge of some kind.",
+]
+
 trap_messages = [
     "You get bit by a spider.",
     "You trip on a dead body and fall to the ground.",
